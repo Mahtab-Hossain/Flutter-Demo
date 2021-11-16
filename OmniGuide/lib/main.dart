2644d3be849619282dbd8462a4834028147d7ffc
alin_alok <alokmahtab88@gmail.com>
2021-11-16 23:43:31 +0600

    inital commit

---
+++ OmniGuide/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('this is the App Bar'),
+        ),
+        body: Text('This is the body'),
+      ),
+    );
+  }
+}
